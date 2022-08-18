2f982313e872ab85e237334e2ef05ccbdd763300
prakash <pt6769759@gmail.com>
2022-08-18 14:22:31 +0545

    makit better

--- src/com/todo.js
+++ src/com/todo.js
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Form from './form';
 
 export default function Todo(){
 
+
+const nameref = useRef();
 const [list, setList] = useState(null);
 const [detail, setDetail] = useState(null);
 const [dialog, setDialog] = useState(true);
+const [nameedit, setEditModeForName] = useState(false);
+const [editemail, setEditemail] = useState(false);
+const [editphone, setEditphone] = useState(false);
 
 useEffect(()=>{
 
@@ -30,6 +35,11 @@ function getDetail(e){
    console.log(result);
    setDialog(false);
    setDetail(result);
+}
+
+function editModename(e){
+  setEditModeForName(true);
+  
 }
 
 
@@ -59,14 +69,21 @@ list.map(e=>
 <div className="dialog-box" hidden={dialog}>
    <button className="cl-btn" onClick={ ()=>{ setDialog(true)  }}>X</button> 
   <div className="name">
-      <div>Name: </div><div> { (detail) ? detail.name: null }</div> 
+      <div>Name: </div>
+     { (nameedit) ? <input type="text"/> : <div> { (detail) ? detail.name: null }</div> } 
+     { (!nameedit) ? <span onClick={ editModename }>edit</span>
+      : <button>Update</button> } 
    </div>
    <div className="email">
-     <div> Email:</div><div> { (detail) ? detail.email: null }</div> 
+     <div> Email:</div>
+    { (editemail) ? <input type="text"/>:  <div> { (detail) ? detail.email: null }</div>}
+    { (!editemail)? <span onClick={ ()=>{ setEditemail(true) } }>edit</span> : <button>Update</button> } 
    </div>
    <div className="phone">
-      <div>Phone:</div><div> { (detail) ? detail.phone: null }</div> 
-   </div>
+      <div>Phone:</div>
+      { (editphone)? <input type="text"/> : <div> { (detail) ? detail.phone: null }</div>} 
+      { (!editphone)? <span onClick={()=>{setEditphone(true) } }>edit</span>: <button>Update</button> }  
+  </div>
 </div>
 </div>
 
